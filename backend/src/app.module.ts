73ea0620a0f39cd042d3b3c27bda3dc7c1234c5a
Egor Koleda <radioegor146@ctf.by>
2024-06-26 11:03:39 +0300

    fix cron scheduler

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,11 @@ import {AppConfigModule} from "./common/config/app-config.module";
 import {DatabaseModule} from "./common/database/database.module";
 import {DomainsModule} from "./domains/domains.module";
 import {MessagesModule} from "./mail/messages.module";
+import {ScheduleModule} from "@nestjs/schedule";
 
 @Module({
   imports: [
+      ScheduleModule.forRoot(),
       AppConfigModule,
       DatabaseModule,
       DomainsModule,
